27bc03287ba6dbab2fcb2fd5161f01d029922d08
Alex Kozik <ajk333@cornell.edu>
2024-07-06 13:55:28 -0400

    Setting up the repo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,31 @@
 
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD_i3HIL4SYkf2AEmKWs2dunhq7m1Glbc4",
+  authDomain: "coursesphere-8bd9a.firebaseapp.com",
+  projectId: "coursesphere-8bd9a",
+  storageBucket: "coursesphere-8bd9a.appspot.com",
+  messagingSenderId: "728789297654",
+  appId: "1:728789297654:web:6b699e8b5ebebc8a8d3fd7",
+  measurementId: "G-PM7PMGBE0R"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
+
+
 function App() {
 
   return (
@@ -9,3 +36,4 @@ function App() {
 }
 
 export default App
+
